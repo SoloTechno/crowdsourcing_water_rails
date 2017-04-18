c0e724715cdc6bb945022f8d48fce131f7ca2bc8
Joshua Wang <joshua31101@gatech.edu>
2017-04-17 22:13:58 -0400

    water purity report submit page

--- app/controllers/water_purity_reports_controller.rb
+++ app/controllers/water_purity_reports_controller.rb
@@ -1,16 +1,25 @@
 class WaterPurityReportsController < ApplicationController
 	before_action :logged_in_user, except: [:index, :show]
 	before_action :set_user, except: [:index, :show]
+	before_action :create_water_purity_report, only: [:new]
 	before_action :correct_user, except: [:index, :show]
-	before_action :set_water_purity_report_from_user, only: [:new]
-	before_action :set_user, except: [:index, :show]
+	before_action :check_user_name, only: [:new]
 
+	# shows both water source/purity reports
 	def index
+		respond_to do |format|
+			format.html { render :index }
+
+			# on AJAX map request
+			format.json {
+				render json: User.get_map_data
+			}
+		end
 	end
 
 	def create
 	end
-
+ 	
 	def new
 	end
 
@@ -21,6 +30,10 @@ class WaterPurityReportsController < ApplicationController
 	end
 	
 	def update
+		@water_purity_report = WaterPurityReport.find(params[:id])
+		if @water_purity_report.update(water_purity_reports_params)
+			redirect_to water_reports_path
+		end
 	end
 
 	def destroy
@@ -28,15 +41,30 @@ class WaterPurityReportsController < ApplicationController
 
 	private
 
-	def correct_user
+		def correct_user
       redirect_to root_url unless current_user?(@user)
     end
 
-	def set_water_purity_report_from_user
-		@water_purity_report = WaterPurityReport.create(user_id: @user.id)
-	end
+		def create_water_purity_report
+			@water_purity_report = WaterPurityReport.create(user_id: @user.id)
+		end
 
-	def set_user
-    @user = current_user
-  end
+		def set_user
+			@user = current_user
+	  end
+
+	  def check_user_name
+	  	if @user.first_name.blank? || @user.last_name.blank?
+	  		msg = "Please type in the first/last name to submit a water report."
+	      redirect_to edit_user_path(@user.id), flash: { error: msg }
+	    end
+	  end
+
+	  def water_purity_reports_params
+	  	params.require(:water_purity_report).permit(
+	  		:location, :lat, :lng, :reporter_name,
+	  		:water_condition, :virus_ppm, :contaminant_ppm,
+  		)
+	  end
+
 end
--- app/controllers/water_source_reports_controller.rb
+++ app/controllers/water_source_reports_controller.rb
@@ -1,7 +1,7 @@
 class WaterSourceReportsController < ApplicationController
 	before_action :logged_in_user, except: [:index, :show]
 	before_action :set_user, except: [:index, :show]
-	before_action :create_water_purity_report, only: [:new]
+	before_action :create_water_source_report, only: [:new]
 	before_action :correct_user, except: [:index, :show]
 	before_action :check_user_name, only: [:new]
 
@@ -45,7 +45,7 @@ class WaterSourceReportsController < ApplicationController
       redirect_to root_url unless current_user?(@user)
     end
 
-		def create_water_purity_report
+		def create_water_source_report
 			@water_source_report = WaterSourceReport.create(user_id: @user.id)
 		end
 
